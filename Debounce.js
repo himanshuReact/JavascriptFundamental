bf58a99943d5015b0ebec4f65774258d6290c1c8
Himanshu Shekhar <74451686+himanshuReact@users.noreply.github.com>
2022-06-18 11:23:34 +0530

    Create Debounce.js

---
+++ Debounce.js
@@ -0,0 +1,27 @@
+const getData = ()=>{
+  console.log('fetching data by calling api')
+  console.log({counter})
+}
+
+
+let counter = 0
+
+function getDebounce(func, delay){
+  let timer;
+  
+  return function(){
+    clearTimeout(timer);
+    
+    let context = this;
+    let args = arguments;
+    timer = setTimeout(()=> func.apply(context,args))
+  }
+}
+
+
+const handleDebounce = getDebounce(getData,1000)
+
+handleDebounce();
+handleDebounce();
+handleDebounce();
+handleDebounce();
